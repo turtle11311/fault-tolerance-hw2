021b61d2f4b938b49d1c23dc9a32eff0d4c96ca6
pingggg0629 <ping326501@gmail.com>
2022-04-07 18:50:34 +0800

    Init voter.py

--- voter.py
+++ voter.py
@@ -1,6 +1,8 @@
 from __future__ import print_function
 import base64
 from os import path
+import time
+from google.protobuf.timestamp_pb2 import Timestamp
 from nacl.public import PrivateKey
 from nacl.signing import SigningKey, VerifyKey
 from nacl.encoding import Base64Encoder
@@ -52,6 +54,49 @@ def run():
             ))
             if rsp.value != b'':
                 logging.info('authorization successs')
+        except grpc.RpcError as e:
+            logging.error(e)
+
+        try:
+            Election_stub = voting_pb2_grpc.eVotingStub(channel)
+            message = Timestamp()
+            message.FromJsonString('2023-01-01T00:00:00Z')
+            election_status = Election_stub.CreateElection(voting_pb2.Election(
+                name='Election2',
+                groups= {'student','teacher'},
+                choices= {'number1','number2'},
+                end_date= Timestamp(seconds=message.seconds,nanos=message.nanos),
+                token = voting_pb2.AuthToken(value=b'01234')))
+            if election_status.code==0:
+                logging.info('Election created successfully')
+            elif election_status.code==1:
+                logging.warning('Invalid authentication token')
+            elif election_status.code==2:
+                logging.warning('Missing groups or choices specification')
+            else:
+                logging.warning('Unknown error')
+        except grpc.RpcError as e:
+            logging.error(e)
+
+        try:
+            CastVote_stub = voting_pb2_grpc.eVotingStub(channel)
+            castVote_status = CastVote_stub.CastVote(voting_pb2.Vote(
+                election_name='Election1',
+                choice_name = 'number1',
+                token = voting_pb2.AuthToken(value=b'01234')))
+            #logging.info(castVote_status)
+        except grpc.RpcError as e:
+            logging.error(e)
+
+        try:
+            GetResult_stub = voting_pb2_grpc.eVotingStub(channel)
+            getResult = GetResult_stub.GetResult(voting_pb2.ElectionName(name='Election1'))
+            if getResult.status:
+                logging.warning('Non-existent election or')
+                logging.warning('The election is still ongoing. Election result is not available yet')
+            else:
+                for i in range(len(getResult.count)):
+                    print('choice name [{}] : {}'.format(getResult.count[i].choice_name, getResult.count[i].count))
         except grpc.RpcError as e:
             logging.error(e)
 
